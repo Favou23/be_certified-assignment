3adc0b8275f88fd19fa19dd7e61f2c4e07aa3b25
favour <shaibfavour26@gmail.com>
2026-01-16 18:53:36 +0100

    initial commmit

---
+++ intro.js
@@ -0,0 +1,7 @@
+const name = "Favour";
+const age = 21;
+const favouriteProgrammingLanguage = "Python";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Favourite Programming Language:", favouriteProgrammingLanguage);
